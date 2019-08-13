1687c50f20a51c9d6f800a249c0cc4a2637655e9
1ak31sha <lakeisha.pogrzeba@gmail.com>
2019-08-13 14:12:25 -0400

    Updates

---
+++ precache-manifest.fb7eb516e431ad2200ace04ee9a83b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b3d54a43b9454058b40bb1b0d37350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47837bc4f19163815a8e",
+    "url": "/static/css/2.48db9afd.chunk.css"
+  },
+  {
+    "revision": "61e43f55472b05078732",
+    "url": "/static/css/main.b46d74f6.chunk.css"
+  },
+  {
+    "revision": "47837bc4f19163815a8e",
+    "url": "/static/js/2.40515655.chunk.js"
+  },
+  {
+    "revision": "61e43f55472b05078732",
+    "url": "/static/js/main.09ae3ad3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
