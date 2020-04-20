7e673ba9bb676e5d6543437c09ef949061a2c7d2
1ak31sha <lakeisha.pogrzeba@gmail.com>
2020-04-19 23:30:40 -0400

    switching to yarn? wip on arrowbox

--- src/components/ArrowBox/ArrowBox.js
+++ src/components/ArrowBox/ArrowBox.js
@@ -1,32 +1,106 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './ArrowBox.css'
-// import ReactLogo from  './action-popup-frame.svg'
-class Button extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div class="flexbox">
-          <div class="arrow_box">test</div>
-        </div>
-      </React.Fragment>
-    )
+import React from "react"
+import PropTypes from "prop-types"
+import { css, cx } from "emotion"
+import "./ArrowBox.css"
+const arrowSize = 10
+const borderSize = 1
+const ArrowBox = ({
+  children,
+  position,
+  size,
+  color,
+  borderWidth,
+  borderColor,
+}) => {
+  let left,
+    bottom,
+    topp,
+    right,
+    borderBottomColor,
+    borderTopColor,
+    borderLeftColor,
+    borderRightColor,
+    marginTop,
+    marginRight,
+    marginBottom,
+    marginLeft = undefined
+
+  switch (position) {
+    case "TOP":
+      left = "50%"
+      bottom = "100%"
+      borderBottomColor = borderColor
+      marginLeft = arrowSize
+      break
+    case "RIGHT":
+      left = "100%"
+      topp = "50%"
+      borderLeftColor = borderColor
+      marginTop = arrowSize
+      break
+    case "BOTTOM":
+      break
+    case "LEFT":
+      break
+    default:
+      break
   }
+
+  return (
+    <div
+      className={css`
+        position: relative;
+        background: ${color};
+        border: ${borderSize}px solid ${borderColor};
+        height: 100px;
+        width: 200px;
+        &:after,
+        &:before {
+          left: ${left};
+          bottom: ${bottom};
+          right: ${right};
+          top: ${topp};
+          border: solid transparent;
+          content: " ";
+          height: 0;
+          width: 0;
+          position: absolute;
+          pointer-events: none;
+        }
+        &:after {
+          border-bottom-color: ${color};
+          border-left-color: ${borderLeftColor};
+          border-width: ${arrowSize}px;
+          margin-left: -${marginLeft}px;
+          margin-top: -${marginTop}px;
+        }
+        &:before {
+          border-bottom-color: ${borderColor};
+          border-left-color: ${borderLeftColor};
+          border-width: ${arrowSize + borderSize}px;
+          margin-left: -${marginLeft + borderSize}px;
+          margin-top: -${marginTop + borderSize}px;
+        }
+      `}
+    >
+      {children}
+    </div>
+  )
 }
 
-Button.propTypes = {
+ArrowBox.propTypes = {
   /** the text to show on the button */
   text: PropTypes.string,
 
   theme: PropTypes.object,
 }
 
-Button.defaultProps = {
-  text: 'Click me',
+ArrowBox.defaultProps = {
+  text: "Click me",
 
   theme: {
     button: {},
   },
 }
 
-export default Button
+export default ArrowBox
--- src/docs/examples/ArrowBox/ArrowBox.examples.js
+++ src/docs/examples/ArrowBox/ArrowBox.examples.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import ArrowBox from '@1ak31sha/ui/ArrowBox'
+import React from "react"
+import ArrowBox from "@1ak31sha/ui/ArrowBox"
 function ArrowBoxContainer() {
-
   return (
     <div>
-      <ArrowBox />
+      <ArrowBox
+        position={"TOP"}
+        size={2}
+        color={"white"}
+        borderWidth={1}
+        borderColor={"palevioletred"}
+      >
+      Brought to you by Lakeisha..........
+      </ArrowBox>
+      <ArrowBox
+        position={"RIGHT"}
+        size={2}
+        color={"white"}
+        borderWidth={1}
+        borderColor={"palevioletred"}
+      >
+      Brought to you by Lakeisha..........
+      </ArrowBox>
     </div>
   )
 }
