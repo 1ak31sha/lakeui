f18c3b870ba49ef0d3d668911c5c20b69bebbeae
1ak31sha <lakeisha.pogrzeba@gmail.com>
2020-07-31 16:26:34 -0400

    pokemon table updates

--- src/components/ReactTable/ReactTable.js
+++ src/components/ReactTable/ReactTable.js
@@ -25,7 +25,7 @@ const Table = ({ columns, data, rowClickCallback }) => {
 
   return (
     <table {...getTableProps()}>
-      <thead>
+      <thead className={css``}>
         {headerGroups.map(headerGroup => (
           <tr
             {...headerGroup.getHeaderGroupProps()}
@@ -33,6 +33,7 @@ const Table = ({ columns, data, rowClickCallback }) => {
               background-image: linear-gradient(#969899, #ccc);
               color: #000;
               th {
+                padding: 5px;
                 &:first-child {
                   border-top-left-radius: 52px;
                   border-bottom-left-radius: 52px;
@@ -40,6 +41,7 @@ const Table = ({ columns, data, rowClickCallback }) => {
                 &:last-child {
                   border-top-right-radius: 52px;
                   border-bottom-right-radius: 52px;
+                  padding-right: 15px;
                 }
               }
             `}
@@ -64,7 +66,7 @@ const Table = ({ columns, data, rowClickCallback }) => {
           return (
             <tr
               {...row.getRowProps()}
-              onClick={()=>rowClickCallback(row)}
+              onClick={() => rowClickCallback(row)}
               className={css`
                 &:first-child {
                   td {
@@ -78,11 +80,177 @@ const Table = ({ columns, data, rowClickCallback }) => {
               `}
             >
               {row.cells.map(cell => {
-                return <td 
-                 className={css`
-                   text-align: center;
-                   `}  
-                {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                let color,
+                  backgroundImage,
+                  textTransform,
+                  textShadow,
+                  fontFamily,
+                  fontWeight,
+                  fontStyle,
+                  fontSize,
+                  padding = undefined
+                if (cell.column.id === "type") {
+                  padding = "3px"
+                  switch (cell.value) {
+                    case "grass":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#a0ce90, #2bb452)"
+                      break
+                    case "psychic":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#b3a3c5, #6b4a9b)"
+                      break
+                    case "water":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#56B2F8, #C0E6F4)"
+                      break
+                    case "fire":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#EA3D4A, #DF9371)"
+                      break
+                    case "lightning":
+                      color = "#000"
+                      backgroundImage = "linear-gradient(#EDB82D, #E8E01D)"
+                      break
+                    case "dark":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#436e88, #30343d)"
+                      break
+                    case "steel":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#7e7c77, #d6c6af)"
+                      break
+                    case "fairy":
+                      color = "#fff"
+                      backgroundImage = "linear-gradient(#fd149b, #c0054a)"
+                      break
+                    case "fighting":
+                      color = "#000"
+                      backgroundImage = "linear-gradient( #e36229, #FFC271)"
+                      break
+                    case "normal":
+                      color = "#000"
+                      backgroundImage = "linear-gradient(#E2D2DC, #EDE9E8)"
+                      break
+
+                    default:
+                      break
+                  }
+                  console.log(cell)
+                } else if (cell.column.id === "name") {
+                  fontFamily = "Fira Sans"
+                } else if (cell.column.id === "hp") {
+                  fontFamily = "Fira Sans"
+                } else if (cell.column.id === "retreatCost") {
+                  fontFamily = "Fira Sans"
+                } else if (cell.column.id === "quantity") {
+                  fontFamily = "Fira Sans"
+                } else if (cell.column.id === "category") {
+                  padding = "3px"
+                  switch (cell.value) {
+                    case "Trainer":
+                      textTransform = "uppercase"
+                      backgroundImage =
+                        "linear-gradient(-90deg,#EEC035, #F3E383,#EEC035)"
+                      color = "#000"
+                      fontFamily = "Signika"
+                      fontStyle = "bold"
+                      fontWeight = "800"
+
+
+                      break
+                    case "Stadium":
+                      textTransform = "uppercase"
+                      backgroundImage = "linear-gradient(#647757,#DEEBD4 )"
+                      color = "Black"
+                      fontWeight = "800"
+                      // fontSize = "10pt"
+                      break
+                    case "Supporter":
+                      textTransform = "uppercase"
+                      color = "#F63916"
+                      backgroundImage =
+                        "linear-gradient(#8A7871, #FBF7E9,#8A7871)"
+                      fontWeight = 600
+                      textShadow = "-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff"
+                      fontFamily = "'Passion One', sans-serif"
+                      fontStyle = "italic"
+                      break
+                    case "Stage 1":
+                      textTransform = "uppercase"
+                      color = "#626565"
+                      backgroundImage =
+                        "linear-gradient(#A19D90, #FFFEFE,#A19D90)"
+                      fontWeight = 800
+                      break
+                    case "Stage 2":
+                      textTransform = "uppercase"
+                      color = "#626565"
+                      backgroundImage =
+                        "linear-gradient(#A19D90, #FFFEFE,#A19D90)"
+
+                      fontWeight = 800
+                      break
+                    case "Basic":
+                      textTransform = "uppercase"
+                      color = "#626565"
+                                            fontFamily = "'Orbitron', cursive"
+                      backgroundImage =
+                        "linear-gradient(#A19D90, #FFFEFE,#A19D90)"
+                      fontWeight = 800
+                      break
+                    case "Tool":
+                      textTransform = "uppercase"
+                      color = "#0E74BA"
+                      backgroundImage =
+                        "linear-gradient(#A19D90, #FFFEFE,#A19D90)"
+                      fontWeight = 800
+                      break
+                    case "Energy":
+                      textTransform = "uppercase"
+                      fontFamily = "'Audiowide', cursive"
+                      color = "#000"
+                      fontWeight = 800
+                      break
+                    case "Special Energy":
+                      textTransform = "uppercase"
+                      fontFamily = "'Audiowide', cursive"
+                      color = "#000"
+                      fontWeight = 500
+                      fontSize = "10pt"
+                      break
+
+                    default:
+                      break
+                  }
+                } else {
+                  padding = "5px"
+                }
+                return (
+                  <td
+                    className={css`
+                      text-align: center;
+                    `}
+                    {...cell.getCellProps()}
+                  >
+                    <div
+                      className={css`
+                        color: ${color};
+                        background-image: ${backgroundImage};
+                        border-radius: 50px;
+                        padding: ${padding};
+                        font-family: ${fontFamily};
+                        text-transform: ${textTransform};
+                        text-shadow: ${textShadow};
+                        font-weight: ${fontWeight};
+                        font-size: ${fontSize};
+                        font-style: ${fontStyle};
+                      `}
+                    >
+                      {cell.render("Cell")}
+                    </div>
+                  </td>
+                )
               })}
             </tr>
           )
--- src/docs/examples/ReactTable/ReactTable.examples.js
+++ src/docs/examples/ReactTable/ReactTable.examples.js
@@ -5,14 +5,31 @@ function TableExample() {
     () => [
       { Header: "#", accessor: "quantity" },
       { Header: "Name", accessor: "name" },
+      { Header: "Set", accessor: "set" },
+      { Header: "Category", accessor: "category" },
       { Header: "Type", accessor: "type" },
+      { Header: "HP", accessor: "hp" },
+      { Header: "Retreat Cost", accessor: "retreatCost" },
     ],
     [],
   )
   const data = React.useMemo(() => [
-    { quantity: 4, name: "Gengar", type: "psychic" },
-    { quantity: 4, name: "Vileplume", type: "grass" },
-    { quantity: 4, name: "Proffessor Oak", type: "trainer" },
+    { quantity: 4, name: "Gengar", set: "SF", category: "Stage 2", type: "psychic", hp: 90, retreatCost: 0 },
+    { quantity: 4, name: "Vileplume", type: "grass", category: "Stage 2", hp: 120, retreatCost: 2 },
+    { quantity: 4, name: "Gyarados", type: "water", category: "Stage 1", hp: 130, retreatCost: 3 },
+    { quantity: 4, name: "Charizard", type: "fire", category: "Stage 2", hp: 120, retreatCost: 4 },
+    { quantity: 4, name: "Raichu", type: "lightning", category: "Stage 1", hp: 110, retreatCost: 0 },
+    { quantity: 4, name: "Rayquaza", type: "normal", category: "Basic", hp: 150, retreatCost: 4 },
+    { quantity: 4, name: "Sneasle", type: "dark", category: "Basic", hp: 60, retreatCost: 0 },
+    { quantity: 4, name: "Durant", type: "steel", category: "Basic", hp: 80, retreatCost: 1 },
+    { quantity: 4, name: "Jigglypuff", type: "fairy", category: "Basic", hp: 120, retreatCost: 1 },
+    { quantity: 4, name: "Rhyperior", type: "fighting", category: "Stage 2", hp: 180, retreatCost: 4 },
+    { quantity: 4, name: "Proffessor Oak", category: "Trainer" },
+    { quantity: 4, name: "Pokemon Collector", category: "Supporter" },
+    { quantity: 4, name: "Broken Time Space", category: "Stadium" },
+    { quantity: 4, name: "Golden Berry", category: "Tool" },
+    { quantity: 4, name: "Water", category: "Energy" },
+    { quantity: 4, name: "Double Colorless", category: "Special Energy" },
   ])
 
   const rowClick = (row) => {
